e141fb6bbd21461eb13d3be87c7d82a7ac03c0de
Tyler Thompson <tylerthompson1492@gmail.com>
2018-12-01 11:19:24 -0500

    Some Quick Upgrade Fixes

---
+++ Assets/Scripts/Upgrade System/Effects/BonusGunUpgradeEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gun Upgrade Effect", menuName = "Upgrade/GunUpgrade")]
+public class BonusGunUpgradeEffect : UpgradeEffect
+{
+    public int NumberOfShots;
+
+    public override void AddEffect(Tank tank)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void RemoveEffect(Tank tank)
+    {
+        throw new System.NotImplementedException();
+    }
+}
