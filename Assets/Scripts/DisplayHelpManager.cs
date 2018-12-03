afdd411fcd801f8e1dd3b57aa16c36f6c5c6a9e2
Tyler Thompson <tylerthompson1492@gmail.com>
2018-12-03 13:37:14 -0500

    UI Changes

---
+++ Assets/Scripts/DisplayHelpManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayHelpManager : MonoBehaviour {
+
+    private bool prevPauseState;
+
+    public void DisplayHelpPanel()
+    {
+        prevPauseState = GameState.GameIsPaused;
+
+        GameState.PauseLevel();
+        this.gameObject.SetActive(true);
+    }
+
+    public void ExitHelpMenu()
+    {
+        this.gameObject.SetActive(false);
+
+        if(prevPauseState == false)
+        {
+            GameState.UnPauseLevel();
+        }
+    }
+
+    public void ToggleMenu()
+    {
+        if(this.gameObject.activeInHierarchy)
+        {
+            ExitHelpMenu();
+        }
+        else
+        {
+            DisplayHelpPanel();
+        }
+    }
+}
