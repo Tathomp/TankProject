0e123d9535fafd44aa69ef56a2147b4e92991e6a
Tyler Thompson <tylerthompson1492@gmail.com>
2018-12-02 21:48:24 -0500

    Esc brings up the pause menu

---
+++ Assets/HUDMenuButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDMenuButtonManager : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            gameObject.GetComponent<Button>().onClick.Invoke();
+        }
+	}
+}
